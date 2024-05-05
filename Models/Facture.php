23f24bdf9c7cd465d137fc308ce30ae7f666e41f
Thierry Leung <ahkhiat@hotmail.com>
2024-05-05 19:24:01 +0200

    facture show ok

--- Models/Facture.php
+++ Models/Facture.php
@@ -43,7 +43,10 @@ class Facture extends Model
 
         try {
             $requete = $this->bd->prepare('SELECT f.id, date, prix_ht, prix_ttc, c.nom AS client_nom, c.prenom AS client_prenom, 
-                                           u.nom AS user_nom, u.prenom AS user_prenom, c.adresse1, c.adresse2, c.code_postal, c.ville, c.email, c.telephone
+                                           u.nom AS user_nom, u.prenom AS user_prenom, 
+                                           c.adresse1, c.adresse2, c.code_postal, 
+                                           c.ville, c.email, c.telephone,
+                                           SUM(prix_ttc - prix_ht) AS total_tva
                                            FROM facture f
                                            JOIN clients c ON f.id_client = c.id
                                            JOIN user u ON f.id_user = u.id
@@ -61,7 +64,14 @@ class Facture extends Model
     {
 
         try {
-            $requete = $this->bd->prepare('SELECT * FROM ligne_facture WHERE id_facture = :fid');
+            $requete = $this->bd->prepare('SELECT l.quantite, l.id_produit, l.id_facture,
+                                            p.name, p.reference, p.price_ht,
+                                            SUM(l.quantite * p.price_ht) AS total_ht
+                                           FROM ligne_facture l 
+                                           JOIN produits p
+                                           ON l.id_produit = p.id
+                                           WHERE id_facture = :fid
+                                           GROUP BY l.quantite, l.id_produit, l.id_facture, p.name, p.reference, p.price_ht;');
 
             $requete->execute(array(':fid'=>$_POST['fid']));
             
--- Views/Facture/view_facture_show.php
+++ Views/Facture/view_facture_show.php
@@ -1,14 +1,14 @@
 <?php
-var_dump($facture);
-var_dump($lignes);
+// var_dump($facture);
+// var_dump($lignes);
 ?>
 <div class="page-content container">
     <div class="page-header text-blue-d2">
         <h1 class="page-title text-secondary-d1">
-            Invoice
+            Facture
             <small class="page-info">
                 <i class="fa fa-angle-double-right text-80"></i>
-                ID: #111-222
+                <?= $facture[0]->id ?>
             </small>
         </h1>
 
@@ -30,11 +30,22 @@ var_dump($lignes);
         <div class="row mt-4">
             <div class="col-12 col-lg-12">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-8 border">
                         <div class=" text-150">
 							<img src="./Content/img/pedalejoyeuse.png" alt="" class="src" width="300">
                         </div>
+						
                     </div>
+					<div class="col-4 border text-sm text-grey-m2 align-middle d-flex flex-column justify-content-end align-items-end">
+						<div>18 rue de la République</div>
+						<div>13002 Marseille</div>
+						<div><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> 04912345678 </div>
+						<div>contact@pedale-joyeuse.fr</div>
+						
+						
+
+
+					</div>
                 </div>
                 <!-- .row -->
 
@@ -77,9 +88,9 @@ var_dump($lignes);
 
                             <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID : </span><?= $facture[0]->id ?></div>
 
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Issue Date:</span> <?= date('d/m/Y', strtotime($facture[0]->date)) ?></div>
+                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Date:</span> <?= date('d/m/Y', strtotime($facture[0]->date)) ?></div>
 
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div>
+                            <!-- <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div> -->
                         </div>
                     </div>
                     <!-- /.col -->
@@ -89,50 +100,37 @@ var_dump($lignes);
                     <div class="row text-600 text-white bgc-default-tp1 py-25">
                         <div class="d-none d-sm-block col-1">#</div>
                         <div class="col-9 col-sm-5">Description</div>
-                        <div class="d-none d-sm-block col-4 col-sm-2">Qty</div>
-                        <div class="d-none d-sm-block col-sm-2">Unit Price</div>
-                        <div class="col-2">Amount</div>
+                        <div class="d-none d-sm-block col-4 col-sm-2">Qté</div>
+                        <div class="d-none d-sm-block col-sm-2">P.U. H.T</div>
+                        <div class="col-2">Total H.T.</div>
                     </div>
 
                     <div class="text-95 text-secondary-d3">
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1">1</div>
-                            <div class="col-9 col-sm-5">Domain registration</div>
-                            <div class="d-none d-sm-block col-2">2</div>
-                            <div class="d-none d-sm-block col-2 text-95">$10</div>
-                            <div class="col-2 text-secondary-d2">$20</div>
-                        </div>
-
-                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                            <div class="d-none d-sm-block col-1">2</div>
-                            <div class="col-9 col-sm-5">Web hosting</div>
-                            <div class="d-none d-sm-block col-2">1</div>
-                            <div class="d-none d-sm-block col-2 text-95">$15</div>
-                            <div class="col-2 text-secondary-d2">$15</div>
-                        </div>
+						<?php  
+						$i = 1;
+						foreach($lignes as $u ): ?>
 
                         <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1">3</div>
-                            <div class="col-9 col-sm-5">Software development</div>
-                            <div class="d-none d-sm-block col-2">--</div>
-                            <div class="d-none d-sm-block col-2 text-95">$1,000</div>
-                            <div class="col-2 text-secondary-d2">$1,000</div>
+                            <div class="d-none d-sm-block col-1"><?= $i ?></div>
+                            <div class="col-9 col-sm-5"><?= $u->name ?></div>
+                            <div class="d-none d-sm-block col-2"><?= $u->quantite ?></div>
+                            <div class="d-none d-sm-block col-2 text-95"><?= $u->price_ht ?> €</div>
+                            <div class="col-2 text-secondary-d2"><?= $u->total_ht ?> €</div>
                         </div>
+						<?php 
+						$i++;
+						endforeach; ?>
 
-                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                            <div class="d-none d-sm-block col-1">4</div>
-                            <div class="col-9 col-sm-5">Consulting</div>
-                            <div class="d-none d-sm-block col-2">1 Year</div>
-                            <div class="d-none d-sm-block col-2 text-95">$500</div>
-                            <div class="col-2 text-secondary-d2">$500</div>
-                        </div>
+							
+
+                        
                     </div>
 
                     <div class="row border-b-2 brc-default-l2"></div>
 
                     <!-- or use a table instead -->
-                    <!--
-            <div class="table-responsive">
+                    
+            <!-- <div class="table-responsive">
                 <table class="table table-striped table-borderless border-0 border-b-2 brc-default-l1">
                     <thead class="bg-none bgc-default-tp1">
                         <tr class="text-white">
@@ -155,39 +153,39 @@ var_dump($lignes);
                         </tr> 
                     </tbody>
                 </table>
-            </div>
-            -->
+            </div> -->
+           
 
                     <div class="row mt-3">
                         <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
-                            Extra note such as company or payment information...
-                        </div>
+							La présente facture est à regler au maximum pour le <?= date('d/m/Y', strtotime($facture[0]->date . ' +1 month')) ?>
+					</div>
 
                         <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
                             <div class="row my-2">
                                 <div class="col-7 text-right">
-                                    SubTotal
+                                    Sous-total
                                 </div>
                                 <div class="col-5">
-                                    <span class="text-120 text-secondary-d1">$2,250</span>
+                                    <span class="text-120 text-secondary-d1"><?= $facture[0]->prix_ht ?> €</span>
                                 </div>
                             </div>
 
                             <div class="row my-2">
                                 <div class="col-7 text-right">
-                                    Tax (10%)
+                                    TVA 
                                 </div>
                                 <div class="col-5">
-                                    <span class="text-110 text-secondary-d1">$225</span>
+                                    <span class="text-110 text-secondary-d1"><?= $facture[0]->total_tva ?> €</span>
                                 </div>
                             </div>
 
-                            <div class="row my-2 align-items-center bgc-primary-l3 p-2">
+                            <div class="row my-2 align-items-center bgc-primary-l3 border ">
                                 <div class="col-7 text-right">
-                                    Total Amount
+                                    Total T.T.C.
                                 </div>
-                                <div class="col-5">
-                                    <span class="text-150 text-success-d3 opacity-2">$2,475</span>
+                                <div class="col-5 border text-bg-light">
+                                    <strong><span class="text-150 text-success-d3 opacity-2"><?= $facture[0]->prix_ttc ?> €</span></strong>
                                 </div>
                             </div>
                         </div>
@@ -196,8 +194,8 @@ var_dump($lignes);
                     <hr />
 
                     <div>
-                        <span class="text-secondary-d1 text-105">Thank you for your business</span>
-                        <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a>
+                        <!-- <span class="text-secondary-d1 text-105">Thank you for your business</span>
+                        <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a> -->
                     </div>
                 </div>
             </div>
